6e3bda31e2fa678deffe6846cfcc99831917b05f
Paul James <paul@peej.co.uk>
2014-05-26 14:21:49 +0100

    Raw document output resource

---
+++ spec/Contentacle/Resources/RawSpec.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace spec\Contentacle\Resources;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class RawSpec extends ObjectBehavior
+{
+    function let(\Tonic\Application $app, \Tonic\Request $request, \Pimple $pimple, \Contentacle\Services\RepoRepository $repoRepo, \Contentacle\Models\Repo $repo)
+    {
+        $repo->loadDocuments('master', 'totem.txt')->willReturn();
+        $repo->prop('document')->willReturn(array('content' => 'totem'));
+        
+        $repoRepo->getRepo('cobb', 'extraction')->willReturn($repo);
+        $pimple->offsetGet('repo_repository')->willReturn($repoRepo);
+
+        $this->beConstructedWith($app, $request);
+        $this->setContainer($pimple);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Contentacle\Resources\Raw');
+    }
+
+    function it_should_show_a_documents_raw_content($repo)
+    {
+        $repo->loadDocuments('master', 'totem.txt')->shouldBeCalled();
+        $response = $this->get('cobb', 'extraction', 'master', 'totem.txt');
+        $response->body->shouldBe('totem');
+    }
+
+}
---
+++ src/Contentacle/Resources/Raw.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Contentacle\Resources;
+
+/**
+ * @uri /users/:username/repos/:repo/branches/:branch/raw/:path
+ */
+class Raw extends Resource {
+
+    function get($username, $repoName, $branch, $path)
+    {
+        $repoRepo = $this->container['repo_repository'];
+
+        $path = $this->fixPath($path, $username, $repoName, $branch, 'raw');
+
+        $repo = $repoRepo->getRepo($username, $repoName);
+        $repo->loadDocuments($branch, $path);
+
+        if ($repo->document) {
+            $response = new \Tonic\Response(200, $repo->document['content']);
+            $response->contentType = 'text/plain';
+            return $response;
+        } else {
+            throw new \Tonic\NotFoundException;
+        }
+    }
+}
